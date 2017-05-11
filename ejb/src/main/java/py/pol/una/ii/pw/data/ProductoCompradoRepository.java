329fbe2d01c7ed9471244e3b5e698c0b48c12597
Cristhian <cristhian.benitez5@gmail.com>
2017-05-10 23:51:05 -0400

    Se borro carrito y memeber

--- ejb/src/main/java/py/pol/una/ii/pw/data/ProductoCompradoRepository.java
+++ ejb/src/main/java/py/pol/una/ii/pw/data/ProductoCompradoRepository.java
@@ -2,18 +2,10 @@ package py.pol.una.ii.pw.data;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.persistence.EntityManager;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
-
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.logging.Logger;
 
 import org.apache.ibatis.session.SqlSession;
-import py.pol.una.ii.pw.mappers.ProductMapper;
 import py.pol.una.ii.pw.mappers.ProductoCompradoMapper;
 import py.pol.una.ii.pw.model.Product;
 import py.pol.una.ii.pw.model.ProductoComprado;
